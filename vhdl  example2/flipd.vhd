ff21ceedbf819814553b0cb5ad532f539cbbd042
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-14 20:22:29 +0430

    Update flipd.vhd

--- vhdl  example2/flipd.vhd
+++ vhdl  example2/flipd.vhd
@@ -5,16 +5,7 @@ entity flipd is
     port (
         d: in STD_LOGIC;
         clk: in STD_LOGIC;
-        clr: in STD_LOGIC;
-        q_out: out STD_LOGIC
-    );
-end flipd;
-
-architecture flipd_arch of flipd is
-begin
-process (clk,clr)
-begin
-if clr='0' then
+        clr: in ST
 q_out <='0';
 elsif (clk'event and clk='1') then
 q_out <=d;
