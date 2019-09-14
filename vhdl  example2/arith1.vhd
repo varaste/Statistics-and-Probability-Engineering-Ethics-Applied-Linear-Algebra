7e5f74ddc872151608a99431e76b96d22714b064
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-14 20:22:23 +0430

    Update arith1.vhd

--- vhdl  example2/arith1.vhd
+++ vhdl  example2/arith1.vhd
@@ -1,10 +1,5 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;	-- (1)
-
-entity arith1 is
-port(
-	a, b :in std_logic_vector(3 downto 0);
+us
 	q : out std_logic_vector(3 downto 0)
 	);
 end;
