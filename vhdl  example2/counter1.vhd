c702c4184e0284dc08cc78ba7b22fbc95a647c34
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-16 19:43:58 +0430

    Update counter1.vhd

--- vhdl  example2/counter1.vhd
+++ vhdl  example2/counter1.vhd
@@ -1,15 +1,4 @@
-library IEEE;
-use IEEE.std_logic
-        enable: in STD_LOGIC;
-        load: in STD_LOGIC;
-        data: in STD_LOGIC_VECTOR (7 downto 0);
-        cnt: inout STD_LOGIC_VECTOR (7 downto 0)
-    );
-end counter1;
-
-architecture counter1_arch of counter1 is
-begin
-count: process(clk,set,reset)
+library IEEE
           begin
             if reset='1' then
                cnt<=(others=>'0');
