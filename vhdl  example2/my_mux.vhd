0802d596af2cd13b5295f26eafd8638da59be0fe
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-16 19:44:13 +0430

    Update my_mux.vhd

--- vhdl  example2/my_mux.vhd
+++ vhdl  example2/my_mux.vhd
@@ -1,14 +1,5 @@
 
---necessary libraries----------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
--------------------------------------------------------------------------------
-entity my_mux is
-port ( a,b : in  std_logic;
-       s : in  std_logic;
-       o : out std_logic);
-end my_mux;
-architecture mux of my_mux is
+--necessary libraries------------
 signal o1,o2: std_logic;
 begin
 o1  <= not(s) and a ;
