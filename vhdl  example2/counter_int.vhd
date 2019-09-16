cbe4aa30e28a3cebad9d3e6842ac6275b09b1c45
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-16 19:44:00 +0430

    Update counter_int.vhd

--- vhdl  example2/counter_int.vhd
+++ vhdl  example2/counter_int.vhd
@@ -2,9 +2,7 @@ library ieee;
 use ieee.std_l
     begin
 counter:process(clk)
-        variable cnt:Integer range 0 to 255;	--(1)
-        begin
-         if (clk'event AND clk='1')then
+     nt AND clk='1')then
 		 cnt:=cnt+1;				--(2)
 		 end if;
 		count<=cnt;
