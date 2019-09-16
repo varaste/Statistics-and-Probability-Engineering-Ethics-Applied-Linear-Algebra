e142bcddaf15d03c9c8bd463de4ece09d844e77d
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-16 19:44:06 +0430

    Update function_ex.vhd

--- vhdl  example2/function_ex.vhd
+++ vhdl  example2/function_ex.vhd
@@ -4,9 +4,4 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity function_ex is  
-   port(a,b,c:in std_logic;
-          cout:out std_logic);
-end entity;
-architecture behavior of function_ex is
---declaring function of carry        
-function carry(bit1,bit2,bit3:in std_l
+   port(a,b,c:in std
