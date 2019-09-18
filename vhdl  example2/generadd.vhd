2549da21c2c2aef4ee833c4df1e8e8694b511af9
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-18 10:21:18 +0430

    Update generadd.vhd

--- vhdl  example2/generadd.vhd
+++ vhdl  example2/generadd.vhd
@@ -4,12 +4,7 @@ use IEEE.std_logic_1164.all;
 entity generadd is
    generic (n:natural:=8);
     port (
-        a: in STD_LOGIC_VECTOR (n-1 downto 0);
-        b: in STD_LOGI
-        sum: out STD_LOGIC_VECTOR
-architecture generadd_arch of generadd is
-    signal c:std_logic_vector (n downto 0);
-begin
+     
    c(0)<=cin;
 gen: for i in 0 to n-1 generate
       sum(i)<=a(i) xor b(i) xor c(i);
