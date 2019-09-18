4fde487ef12269e541ad0454631b26e0e17f5946
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-18 10:21:05 +0430

    Update usigned1.vhd

--- vhdl  example2/usigned1.vhd
+++ vhdl  example2/usigned1.vhd
@@ -4,12 +4,6 @@ use ieee.std_logic_unsigned.all;	-- (1)
 
 entity unsigned1 is
 port(
-	a, b :in std_logic_vector(3 downto 0);
-	q : out std_logic_vector(3 downto 0)
-	);
-end;
-
-architecture behavior of unsigned1 is
-begin
+	a, 
 	q <= a + b;	--(2)
 end;
