0ab39241bf6b1f797745de2fac1f5f26dd9ce489
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-18 10:21:21 +0430

    Update logical_alu.vhd

--- vhdl  example2/logical_alu.vhd
+++ vhdl  example2/logical_alu.vhd
@@ -4,9 +4,7 @@ port ( a,b : in  std_logic_vector(3 downto 0);
        alu_out: out std_logic_vector(3 downto 0));
 end logical_alu;
 
-architecture behav of logical_alu is
-begin
-alu_out  <= (a 
+architecture
        	    (a  xor  b)    when op="100" else
        	    (not a)         when op="110" else
        	    b;
