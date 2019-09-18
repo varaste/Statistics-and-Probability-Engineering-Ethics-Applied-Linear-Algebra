2601942e4c58e4a7d5cf211a24be19acc3091cf0
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-18 10:21:07 +0430

    Update shift3.vhd

--- vhdl  example2/shift3.vhd
+++ vhdl  example2/shift3.vhd
@@ -1,10 +1,4 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
-
-entity shift3 is
-    port (
-        clk,load,seri_lef: in STD_LOGIC;
-        data_in: in STD_LOGIC_VECTOR (7 downto 0);
         qout: buffer STD_LOGIC_VECTOR (7 downto 0)
     );
 end shift3;
