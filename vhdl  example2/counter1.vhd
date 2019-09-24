6bfc222762c760c614f004ee4319615ba89e9a49
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-24 19:26:30 +0330

    Update counter1.vhd

--- vhdl  example2/counter1.vhd
+++ vhdl  example2/counter1.vhd
@@ -3,9 +3,7 @@ library IEEE
             if reset='1' then
                cnt<=(others=>'0');
                elsif set='1' then
-               cnt<=(others=>'1');
-               elsif (clk'event and clk='1') then
-                    if load='1' then
+
                     cnt<=data;
                     elsif enable='1' then
                     cnt<=cnt+1;
