5cbf93700e74ea8d39f1c4c72ef1f1469b5fd06f
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-30 22:46:08 +0330

    Update logical_alu.vhd

--- vhdl  example2/logical_alu.vhd
+++ vhdl  example2/logical_alu.vhd
@@ -1,11 +1,3 @@
 entity logical_alu is
 port ( a,b : in  std_logic_vector(3 downto 0);
-       op : in  std_logic_vector(2 downto 0);
-       alu_out: out std_logic_vector(3 downto 0));
-end logical_alu;
-
-architecture
-       	    (a  xor  b)    when op="100" else
-       	    (not a)         when op="110" else
-       	    b;
-end behav;
+       op : in  std_l
