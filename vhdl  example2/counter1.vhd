daea6945e253545d9a1d579a9d04484bf7e524b0
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-02 09:05:32 +0330

    Update counter1.vhd

--- vhdl  example2/counter1.vhd
+++ vhdl  example2/counter1.vhd
@@ -2,7 +2,4 @@ library IEEE
           begin
             if reset='1' then
                cnt<=(others=>'0');
-               elsif set='1' then
-              end if;
-end process count ;      
-end counter1_arch;
+               elsif set='1' 
