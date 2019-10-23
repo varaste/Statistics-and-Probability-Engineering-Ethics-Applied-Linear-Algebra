f020edc2a2f90fa5d06253119a4cb30b7ae4477a
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-23 19:31:49 +0330

    Update maxwith.vhd

--- vhdl  example2/maxwith.vhd
+++ vhdl  example2/maxwith.vhd
@@ -5,9 +5,3 @@ library IEEE;
 
 architecture maxwith_arch of maxwith is
 begin
-   with s select
-       x<=a when "00",
-          b when "01",
-          c when "10",
-          d when others; 
-end maxwith_arch;
