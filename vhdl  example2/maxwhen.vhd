a7e5ba45e3e9dcdb0f34a3726705ad6f366d200b
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-23 19:31:46 +0330

    Update maxwhen.vhd

--- vhdl  example2/maxwhen.vhd
+++ vhdl  example2/maxwhen.vhd
@@ -7,10 +7,3 @@ use IEEE.std_logic_1164.all;
 entity maxwhen is
     port (
         a: in STD_LOGIC_VECTOR (3 downto 0);
-
-    );
-end maxwhen;
-
-       c when (s="10") else
-       d;
-end maxwhen_arch;
