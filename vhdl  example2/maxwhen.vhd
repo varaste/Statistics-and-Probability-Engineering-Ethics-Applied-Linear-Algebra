e82a92f339a7ba88ab5999bb4a8eac6e54a26696
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-26 23:05:14 +0330

    Delete maxwhen.vhd

--- vhdl  example2/maxwhen.vhd
+++
@@ -1,9 +0,0 @@
---
---multiplexer 4 to 1 (4 bits0) using 'when ----else'
---
-library IEEE;
-use IEEE.std_logic_1164.all;
-
-entity maxwhen is
-    port (
-        a: in STD_LOGIC_VECTOR (3 downto 0);
