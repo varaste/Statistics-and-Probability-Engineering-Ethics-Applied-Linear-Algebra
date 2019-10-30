1afac10db0cb67aeb043bd89894cf2e7de5279db
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-30 19:56:05 +0330

    Update registd.vhd

--- vhdl  example2/registd.vhd
+++ vhdl  example2/registd.vhd
@@ -2,16 +2,5 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity registd is
-        q: out STD_LOGIC_VECTOR (7 downto 0)
-    );
-end registd;
-
-architecture registd_arch of registd is
-begin
-     process (clk)
-         begin 
-              if (clk'event and clk='0') then
-                  q<=d;
-              end if;
-     end process;
+        q: out STD_LOG
 end registd_arch;
