47c4eef4b0cbbaeb2a9caf58d4372d0730e7f65c
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-10-31 16:46:55 +0330

    Update procedure_fa.vhd

--- vhdl  example2/procedure_fa.vhd
+++ vhdl  example2/procedure_fa.vhd
@@ -13,12 +13,3 @@ begin
 process(a,b,cin)
 variable result: std_logic_vector(3 downto 0);	--(1)
 variable carry: std_logic;	--(2)
-begin
-	full_adder(a(0), b(0), cin  , result(0), carry);	--(3)
-	full_adder(a(1), b(1), carry, result(1), carry);	--(4)
-	full_adder(a(2), b(2), carry, result(2), carry);	--(5)
-	full_adder(a(3), b(3), carry, result(3), carry);	--(6)
-	sum <= result;	--(7)
-	cout <= carry;	--(8)
-end process;
-end architecture;
