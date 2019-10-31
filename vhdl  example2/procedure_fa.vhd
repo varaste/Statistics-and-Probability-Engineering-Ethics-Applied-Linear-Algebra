c4467369bbc11f4e2e148a6a6938a76436a6e1cb
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-11-01 00:09:39 +0330

    Update procedure_fa.vhd

--- vhdl  example2/procedure_fa.vhd
+++ vhdl  example2/procedure_fa.vhd
@@ -2,13 +2,6 @@
 --*******Defining  and using procedure******
 
 library ieee;
-use ieee.std_logic_1164.all;
-entity procedure_fa is  
-port(
-	a,b:in std_logic_vector(3 downto 0);
-	cin:in std_logic;
-	sum:out std_lo
---end declaring
 begin
 process(a,b,cin)
 variable result: std_logic_vector(3 downto 0);	--(1)
