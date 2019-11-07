864ccd78ac315de02daac595d18da39c3a8d0d83
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-11-07 13:02:30 +0330

    Update procedure_pack_fa.vhd

--- vhdl  example2/procedure_pack_fa.vhd
+++ vhdl  example2/procedure_pack_fa.vhd
@@ -4,10 +4,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 package my_pack d_logic_vector(3 downto 0);
-	cout:out std_logic);
-end entity;
-architecture proc_pack of procedure_pack_fa is
-	sum <= result;	--(7)
+	cout:out std result;	--(7)
 	cout <= carry;	--(8)
 end process;
 end architecture;
